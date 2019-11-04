03543aa4578ae16b7619ba837ffe84ceb61c6efe
Agbele_Kolade <kolagbele@gmail.com>
2019-11-04 12:32:01 +0100

    socket io setup for sending and recieving messages complete

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
 
+
+const { addUser, removeUser, getUser, getUserInRoom } = require("./users");
+
 const app = express();
 const server = http.createServer(app);
+
+
 const io = socketio(server);
 
 const router = require("./router");
@@ -12,6 +17,26 @@ app.use(router);
 io.on('connection', (socket) => {
   console.log("We have a connection");
 
+  socket.on("join", ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, user });
+
+    if (error) return callback(error);
+
+    socket.emit("message", { user: "admin", text: `${user.name}, Welcome to the room ${user.room}` });
+    socket.broadcast.to(user.room).emit("message", { user: "admin", text: `${user.name} has joined` });
+
+    socket.join(user.room);
+
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+
+    callback();
+  });
 
   socket.on("disconnect", () => {
     console.log("User just left");
