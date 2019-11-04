dcccb63c84ef2b10f2ffea2179c236e0488b8d2e
Agbele_Kolade <kolagbele@gmail.com>
2019-11-04 09:14:35 +0100

    socketio connection initiated

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const io = socketio(server);
 const router = require("./router");
 app.use(router);
 
+io.on('connection', (socket) => {
+  console.log("We have a connection");
+
+
+  socket.on("disconnect", () => {
+    console.log("User just left");
+  })
+})
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`server running on port ${port} `));
